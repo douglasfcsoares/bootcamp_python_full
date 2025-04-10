95d49603323e7e9f95368b082d10d43bf718751b
douglasfcsoares <douglasfcsoares@gmail.com>
2025-04-10 11:41:23 -0300

    Operadores relacionais

---
+++ introducao_python/04_operadores_ralacionais/operadores_relacionais.py
@@ -0,0 +1,13 @@
+# Operadores relacionais
+# == igual
+# != diferente
+# > maior
+# < menor
+# >= maior ou igual
+# <= menor ou igual
+
+
+operador_relacional = 2 == 2
+operador_relacional2 = 2 == 3
+print(operador_relacional)
+print(operador_relacional2)
